90474be38a0dadbfbb26c870282e0768f14202cd
GIT-Saikat <saikatmondal2105@gmail.com>
2025-07-09 21:23:21 +0530

    docker file

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express"
+
+const app =express()
+
+
+app.get("/",(req,res)=>{
+    res.json({
+        "message":"get endpoint"
+    })
+})
+
+app.post("/",(req,res)=>{
+    res.json({
+        "message":"post endpoint"
+    })
+})
+
+app.listen(3000);
